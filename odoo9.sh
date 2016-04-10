4ad9ac965ddb4ae4e15484fbab2e420fb86ddd2c
Eric Geens <ericgeens@clouderp.be>
2016-04-10 22:48:09 +0200

    Update odoo9.sh

--- odoo9.sh
+++ odoo9.sh
@@ -13,7 +13,7 @@
 #odoo
 OE_USER="odoo"
 OE_HOME="/opt/$OE_USER"
-OE_HOME_EXT="/opt/$OE_USER/$OE_USER-server"
+OE_HOME_EXT="/opt/$OE_USER/$OE_USER"
 
 # Enter Odoo version for checkout "9.0" for version 9.0, 8.0, 7.0 (version 7), saas-4, saas-5 (opendays version) and "master" for trunk
 OE_VERSION="9.0"
@@ -44,21 +44,17 @@ sudo sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 
-#--------------------------------------------------
-# Install Dependencies
-#--------------------------------------------------
 echo -e "\n---- Install tool packages ----"
 sudo apt-get install python-pip -y
 
 echo -e "\n---- Install Nodejs and npm ----"
-sudo apt-get install nodejs -y
 sudo apt-get install npm -y
 sudo ln -s /usr/bin/nodejs /usr/bin/node #In later debian (>jessie) and ubuntu (>14.04) add symlink as npm packages call 'node' but debian calls the binary 'nodejs'
 sudo npm install -g less less-plugin-clean-css
 
-echo -e "\n---- Install python packages ----"
-sudo apt-get install python-babel python-pychart python-cups python-dateutil python-decorator python-docutils python-feedparser python-gdata python-geoip python-gevent python-greenlet python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-markupsafe python-mock python-openid python-passlib python-pil python-psutil python-psycopg2 python-pydot python-pyparsing python-pypdf python-tz python-usb python-yaml python-qrcode python-reportlab python-requests python-simplejson python-six python-unicodecsv python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-zsi -y
-sudo pip install jcconv ofxparse suds-jurko
+#echo -e "\n---- Install python packages ----"
+#sudo apt-get install python-babel python-pychart python-cups python-dateutil python-decorator python-docutils python-feedparser python-gdata python-geoip python-gevent python-greenlet python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-markupsafe python-mock python-openid python-passlib python-pil python-psutil python-psycopg2 python-pydot python-pyparsing python-pypdf python-tz python-usb python-yaml python-qrcode python-reportlab python-requests python-simplejson python-six python-unicodecsv python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-zsi -y
+#sudo pip install jcconv ofxparse suds-jurko
 
 echo -e "\n---- Create ODOO system user ----"
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
@@ -72,6 +68,9 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
 sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+
+echo -e "\n---- Intalling dependencies -----"
+pip install -r $OE_HOME/requirements.txt
 
 echo -e "\n---- Create custom module directory ----"
 sudo su $OE_USER -c "mkdir $OE_HOME/custom"
